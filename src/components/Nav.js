7458ea26ba8491c270d3a2ba819f2c366f1d7534
Keith Laptop <Keithblack4290@gmail.com>
2021-07-12 14:33:04 -0600

    navbar rendering, menu btn opens drawer

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import { Drawer, ListItem, ListItemIcon, ListItemText, List, Icons } from '@material-ui/core/';
+import React, { useState, setState } from 'react'
+import { Drawer, ListItem, ListItemIcon, ListItemText, List, Divider, IconButton, AppBar, Toolbar, Typography, Button } from '@material-ui/core/';
 import { makeStyles } from '@material-ui/core/styles'
 import { GetAppRounded, InfoRounded, AccountTreeRounded, EmailRounded } from '@material-ui/icons/';
 import { withRouter } from "react-router-dom"
+import MenuIcon from "@material-ui/icons/Menu"
 
-const useStyles = makeStyles({
+
+const useStyles = makeStyles((theme)=>({
     drawer: {
         width: '200px'
-    }
-})
+    },
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
 
 const Nav = (props) => {
+
+    const [open, setOpen] = useState(false);
+
+
+
     const { history } = props;
     const classes = useStyles();
     const itemList = [
@@ -34,25 +51,47 @@ const Nav = (props) => {
             icon: <GetAppRounded />
         }
     ]
-    return (
-        <div>
-            <Drawer variant="permanent" anchor="left" className={classes.drawer}>
 
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton} color="inherit"
+                        aria-label="menu"
+                        onClick={() => { setOpen(true) }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                anchor="left"
+                open={open}
+                onClose={() => { setOpen(false) }}
+                onOpen={() => { }}
+                className={classes.drawer}>
+                <h1>Photo and Email goes here</h1>
+                <Divider />
                 <List>
                     {itemList.map((item, index) => {
                         const { text, icon, onClick } = item;
                         return (
-                            <ListItem button key={text} onClick= {onClick}>
+                            <ListItem button key={text} onClick={onClick}>
                                 {icon && <ListItemIcon>{icon}</ListItemIcon>}
                                 <ListItemText primary={text} />
                             </ListItem>
                         )
                     })}
                 </List>
-
             </Drawer>
         </div>
     )
 }
 
-export default withRouter (Nav);
+export default withRouter(Nav);
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Landing = () => {
     return (
-       <h1>HEllO</h1>
+        <h1>HEllO</h1>
     )
 }
 
