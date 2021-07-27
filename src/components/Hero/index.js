8276481530712f4b3fa0da03fa13115256b80ff4
Keith Laptop <Keithblack4290@gmail.com>
2021-07-27 17:03:19 -0600

    getting skills together

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,17 +1,21 @@
 import "./style.css"
 import React from 'react'
-import { Grid, Box, Paper, CardMedia, Typography} from "@material-ui/core"
+import { Box, Paper, Typography } from "@material-ui/core"
 
 
 
 
 const Hero = () => {
     return (
-        <Paper id="hero" xs={12} >
-            <CardMedia className="hero-img">
-            
-            <Typography variant="h1" id="about-header" >ABOUT ME</Typography>
-            </CardMedia>
+        <Paper component="div" id="hero" xs={12} >
+            <Box
+                display="flex"
+                alignContent="center"
+                p={1}
+                m={1}
+                sx={{ height: 100 }}>
+                <Typography variant="h1" id="about-header" >ABOUT ME </Typography>
+            </Box>
         </Paper>
     )
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -32,85 +32,113 @@ const About = () => {
     return (
 
         <div>
-            <Hero />
+
             <Container component="div" className={classes.root} style={{ background: "#eee" }}>
 
-
-                <Grid container spacing={1}>
-
-
-
-                    <Grid xs={12} style={{ background: "white" }}>
+                <Hero />
+                <Grid xs={12}>
+                    <Grid xs={6} style={{float:"left"}}>
+                        <Paper style={{ background: "white" }}>
 
 
-                        <Grid >
-                            <Grid xs={2}
-                                style={{ float: "left" }}>
-                                <Avatar
 
-                                    alt="Holiday Party"
-                                    src={Holiday}
-                                    className={classes.large} ></Avatar>
+                            <Grid item >
+                                <Box
+                                    display="flex"
+                                    justifyContent="center"
+                                    pt={5}
+                                >
+                                    <Avatar
+
+                                        alt="Holiday Party"
+                                        src={Holiday}
+                                        className={classes.large} >
+
+                                    </Avatar>
+                                </Box>
                             </Grid>
 
-                            <Grid xs={9} style={{ float: "right" }}>
+                            <Grid  >
                                 <Typography component="div">
-                                    <Box fontSize="fontSize">
+                                    <Box
+                                        fontSize="fontSize"
+                                        textAlign="center"
+                                        px={1}
+                                        mx={10}
+                                        my={1}>
+
                                         <p>Hello, I am Keith, I am a Software Developer with a background in management and business development.
                                             The quality of skills that I have learned in my past professions are proving to be a great benefit to my growth as a Developer.
                                             I am always searching for new ways to improve my programming skills by keeping up to date with new tools and continuing to practice my craft. </p>
-                                    </Box>
 
-                                    <Box fontSize="fontSize">
+
+
                                         <p>I find knowledge to be exciting which is my favorite aspect of programming.
                                             I always look forward to learning new techniques,  languages, tools, or technologies that I can use for a project.
                                             The challenges that come with continuous learning can be frustrating at first but I get a huge sense of fulfillment once I finally find a solution.
                                             Another aspect of programming that I really enjoy that stands out from other professions is the community. There is always someone who is willing to help and grow with you whether that is at a place of business or on a community forum like StackOverflow or Reddit.
                                         </p>
-                                    </Box>
-                                </Typography>
 
 
-                            </Grid>
 
-                            <Grid
-                                xs={4}
-                                style={{ float: "right" }}>
-                                <Avatar
-
-                                    alt="Holiday Party"
-                                    src={Holiday}
-                                    className={classes.large} ></Avatar>
-                            </Grid>
-
-                            <Grid xs={8} style={{ float: "left" }}>
-                                <Typography component="div">
-                                    <Box fontSize="fontSize">
                                         <p>Outside of programming, I like to explore the City of Denver’s restaurants, go to sporting events, and play video games with friends.
                                             I also have a fond love for Comedy, especially stand-up comedy.
                                             I really enjoy catching some of my favorite acts who come through Denver and look forward to new specials.
                                             Feel free to reach out and learn more about me!</p>
                                     </Box>
+
                                 </Typography>
 
 
                             </Grid>
 
-                        </Grid>
 
+
+                        </Paper>
+                    </Grid>
+
+                    <Grid xs={6} style={{float:"right"}}>
+                        <Paper>
+                            <Box>
+                                <Typography
+                                    variant="h2">
+                                    Skills
+                                </Typography>
+                                <Typography >
+                                    <Box>
+                                        <ul>
+                                            <li>Javascripts</li>
+                                            <li>CSS3</li>
+                                            <li>HTML5</li>
+                                            <li>MySql</li>
+                                            <li>MongoDB</li>
+                                            <li>GitHub</li>
+                                            <li>GitLab</li>
+                                            <li>Git</li>
+                                            <li>Bootstrap</li>
+                                            <li>Material UI</li>
+                                            <li>Foundation</li>
+                                            <li>React</li>
+                                            <li>NPM</li>
+                                            <li>JQuery</li>
+                                            <li>Node</li>
+                                            <li>Express</li>
+                                            <li>MERN</li>
+                                        </ul>
+                                    </Box>
+
+                                </Typography>
+                            </Box>
+                        </Paper>
                     </Grid>
 
 
-                    {/* <Grid item xs={6}>
-                        
-                            <AboutCard className={classes.paper} />
-                        
-                    </Grid> */}
-
-
-
-
                 </Grid>
+
+
+
+
+
 
 
             </Container>
